d541ab1df81557b786fa8dd0eedba38eb0321aae
Phineas <phin@phineas.io>
2021-03-27 22:04:03 +0000

    add multisig ownerships

--- src/pages/Etc.tsx
+++ src/pages/Etc.tsx
@@ -1,21 +1,47 @@
-import { Link } from 'react-router-dom';
-import PageWrapper from './PageWrapper';
+import { Link } from "react-router-dom";
+import PageWrapper from "./PageWrapper";
 
 const Etc = () => (
   <PageWrapper>
     <h1>/etc</h1>
     <h2>Thanks</h2>
-    <p>Thanks for visiting. I hope you enjoyed your stay. The source code for this website is available at <a href={"https://github.com/phineas/phineas.io"}>phineas/phineas.io</a></p>
+    <p>
+      Thanks for visiting. I hope you enjoyed your stay. The source code for
+      this website is available at{" "}
+      <a href={"https://github.com/phineas/phineas.io"}>phineas/phineas.io</a>
+    </p>
     <h2>Contact</h2>
-    <p>I'm most responsive through Twitter DMs, you can <a href={"https://twitter.com/messages/compose?recipient_id=102583719"}>click here</a> to DM me on Twitter.</p>
+    <p>
+      I'm most responsive through Twitter DMs, you can{" "}
+      <a href={"https://twitter.com/messages/compose?recipient_id=102583719"}>
+        click here
+      </a>{" "}
+      to DM me on Twitter.
+    </p>
     <h2>Other</h2>
     <ul>
-      <li><Link to={"/presence"}>/presence</Link></li>
-      <li><a href={"https://phin.vc"}>phin.vc</a></li>
-      <li>PGP Fingerprint: <code>D740 E413 3A74 815E F5B6 F11B AF8B EE0C CF1D 0E46</code></li>
-      <li>Primary ETH Address: <code>0xd3AbB464df46568d1E72Fa5F156F05458f1AB26e</code></li>
+      <li>
+        <Link to={"/presence"}>/presence</Link>
+      </li>
+      <li>
+        <a href={"https://phin.vc"}>phin.vc</a>
+      </li>
+      <li>
+        PGP Fingerprint:{" "}
+        <code>D740 E413 3A74 815E F5B6 F11B AF8B EE0C CF1D 0E46</code>
+      </li>
+      <li>
+        Primary ETH Address:{" "}
+        <code>0xd3AbB464df46568d1E72Fa5F156F05458f1AB26e</code>
+      </li>
+      <li>
+        ETH Multisigs I'm a part of:{" "}
+        <ul>
+          <li><a href={"https://gnosis-safe.io/app/#/safes/0x354A70969F0b4a4C994403051A81C2ca45db3615"}>Waifusion Safe</a></li>
+        </ul>
+      </li>
     </ul>
   </PageWrapper>
-)
+);
 
 export default Etc;
