457130d763d166deb7100f3aa4a770fffa0a3516
Phineas <phin@phineas.io>
2023-01-21 00:30:57 +0000

    clean up

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import './index.css';
 import App from './App';
+import './index.css';
 import 'react-tippy/dist/tippy.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root') as HTMLElement);
